2d95501469678be1a3946273db13960ae1c189ab
Debbie Michel and Luciano Oliveira <debbie@>
2017-08-15 10:45:55 -0700

    still working on a forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){ //Get html ready
   $("form").submit(function(e){ //select the form
     e.preventDefault(); //Prevents the form from going somewhere
-    var inputText = $("#text").val(); //create a variable for the input
-    console.log (inputText);
+    var letters = $("#text").val(); //create a variable for inputText
+    function isItAVowel(letters){
+    var vowel =["a", "e", "i", "o", "u"];
+    for (var i=0; i< vowel.length; i++){
+      if (letters === vowel[i]){
+        console.log (letters);
 
+      }
+    }
+  };
+  isItAVowel(letters);
   });
-
 });
+
+
+
+// vowels.forEach(function(vowel){
+//   inputText = $("#text").val();
+//   var inputSingleLetters = inputText.split(""); //split the sentence into singl letters and save to the a variable. Put in a space between the quotation marks in split to split the sentence into words.
+//   if (inputSingleLetters === vowels[vowel]){
+//     console.log(inputSingleLetters);
+//   };
+// });
